49dbf6fac76cdfadff37d80b023a1f080290ac8c
nacho peirano <nacho.peirano@gmail.com>
2019-07-18 20:33:21 -0300

    ServicioCategorias y página categoría logica

--- Frontend/CompraCercaFrontEnd/src/app/categories/categories.page.ts
+++ Frontend/CompraCercaFrontEnd/src/app/categories/categories.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy  } from '@angular/core';
+import { CategoriesService } from './categories.service';
+import { CategoryResponse } from './categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesPage implements OnInit {
 
-  constructor() { }
 
+  fatherCategories: CategoryResponse[] =  [];
+  categoryImage: string ;
+
+  constructor(private categoriesService: CategoriesService) {
+      this.categoryImage = 'assets/assets.png';
+
+   }
   ngOnInit() {
+    this.categoriesService.getFatherCategories().subscribe( (res: CategoryResponse[]) => {
+      this.fatherCategories = res;
+      console.log(res);
+    });
   }
+
 
 }
---
+++ Frontend/CompraCercaFrontEnd/src/app/categories/categories.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse  } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+
+export interface CategoryResponse {
+  father: number;
+  id: number;
+  name: string;
+
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  BASE_URL  = 'http://localhost:3000';
+  LOCATIONS_URL = '/categories';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getFatherCategories(): Observable<CategoryResponse[]> {
+      return this.http.get<CategoryResponse[]>(this.BASE_URL + this.LOCATIONS_URL).pipe(
+        catchError(this.handleError)
+      );
+
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
